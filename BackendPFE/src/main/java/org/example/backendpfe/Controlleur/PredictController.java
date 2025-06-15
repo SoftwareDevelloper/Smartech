e6b6280b44ec95c1a523938f495f0f10fb384400
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:36:18 +0100

    Update PredictController.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/PredictController.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/PredictController.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
+@CrossOrigin(origins = "https://smartech-production-e572.up.railway.app", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
 public class PredictController
 {
     @PostMapping("/predict")
