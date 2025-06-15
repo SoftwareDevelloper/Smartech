7b36a5b4559f09eb0a54a64b8ecca359a8186cdd
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:37:41 +0100

    Update SchoolLevelController.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/SchoolLevelController.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/SchoolLevelController.java
@@ -5,7 +5,7 @@ import org.example.backendpfe.ServiceImpl.SchoolLevelService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="smartech-production-1020.up.railway.app",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/api/school-levels")
 public class SchoolLevelController {
     private final SchoolLevelService schoolLevelService;
