e405f6be9d32f6da599c31f38cdf644113245b1f
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:02:13 +0100

    Update Login.jsx

--- smarttech_frontend/src/Page/Logins/Login.jsx
+++ smarttech_frontend/src/Page/Logins/Login.jsx
@@ -33,7 +33,7 @@ const Login = ({ onLogin }) => {
       }
         console.log('sign up function executed : ' , formData)
         try{
-        const response = await fetch('http://localhost:9000/api/login' ,{
+        const response = await fetch('https://smartech-production-1020.up.railway.app/api/login' ,{
           method:"POST",
           headers :{
             Accept:'application/json',
