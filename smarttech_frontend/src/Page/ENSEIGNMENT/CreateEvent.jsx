24c69668431b34d642bf69a7cf113f9bde471ed7
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 20:59:45 +0100

    Update CreateEvent.jsx

--- smarttech_frontend/src/Page/ENSEIGNMENT/CreateEvent.jsx
+++ smarttech_frontend/src/Page/ENSEIGNMENT/CreateEvent.jsx
@@ -27,7 +27,7 @@ const CreateEvent = () => {
 
   // Fetch events
   useEffect(() => {
-    fetch(`http://localhost:9000/event/GetEvent?lang=${currentLanguage}`)
+    fetch(`https://smartech-production-1020.up.railway.app/event/GetEvent?lang=${currentLanguage}`)
       .then(response => response.json())
       .then(data => {
         if (Array.isArray(data)) {
@@ -120,7 +120,7 @@ const CreateEvent = () => {
     }
     
     try {
-      const response = await fetch('http://localhost:9000/event/createEvent', {
+      const response = await fetch('https://smartech-production-1020.up.railway.app/event/createEvent', {
         method: "POST",
         headers: {
           Accept: 'application/json',
@@ -155,7 +155,7 @@ const CreateEvent = () => {
   const RemoveEvent = async(id) => {
     setEvent(prev => prev.filter(event => event.id !== id));
     if (localStorage.getItem("auth-token")) {
-      fetch(`http://localhost:9000/event/delete/${id}`, {
+      fetch(`https://smartech-production-1020.up.railway.app/event/delete/${id}`, {
         method: "DELETE",
         headers: {
           Accept: 'application/json',
