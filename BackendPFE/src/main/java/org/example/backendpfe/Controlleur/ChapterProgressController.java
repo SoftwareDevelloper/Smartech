f6b66d5f8496d878c57cc5b939a2bcfb25b31664
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:15:20 +0100

    Update ChapterProgressController.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/ChapterProgressController.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/ChapterProgressController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000,http://localhost:3001",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/api/chapter-progress")
 public class ChapterProgressController {
     @Autowired
