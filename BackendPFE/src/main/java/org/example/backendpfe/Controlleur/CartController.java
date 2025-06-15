eff46ab8a0e85b01028400f9b64e537264b53654
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:14:44 +0100

    Update CartController.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/CartController.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/CartController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000,http://localhost:3001",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/api/cart")
 public class CartController {
     @Autowired
