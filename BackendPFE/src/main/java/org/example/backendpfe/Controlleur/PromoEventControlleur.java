23b3f68296c55b197de5157085943a810acb7bce
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:18:03 +0100

    Update PromoEventControlleur.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/PromoEventControlleur.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/PromoEventControlleur.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000,http://localhost:3001",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/event")
 public class PromoEventControlleur {
 
