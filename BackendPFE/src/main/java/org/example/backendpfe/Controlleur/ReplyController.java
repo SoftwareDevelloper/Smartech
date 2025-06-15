92faf9346369a56407891bd3ebc6ccc10118fae7
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:37:33 +0100

    Update ReplyController.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/ReplyController.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/ReplyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="smartech-production-1020.up.railway.app",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/api/v2/replies")
 public class ReplyController {
 
