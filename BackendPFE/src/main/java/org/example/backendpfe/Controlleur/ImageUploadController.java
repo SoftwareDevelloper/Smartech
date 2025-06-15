42e61a1aa0d44b9c81d6fdd6785ba9951591f87a
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:16:11 +0100

    Update ImageUploadController.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/ImageUploadController.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/ImageUploadController.java
@@ -13,7 +13,7 @@ import java.net.MalformedURLException;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000,http://localhost:3001",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("api/v1/file")
 public class ImageUploadController {
     private final ImageUploadService imageUploadService;
