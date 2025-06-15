f2bae88119bbb6ec17ba90df05c97a3a8aecd0c6
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:15:47 +0100

    Update CommentaireControlleur.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/CommentaireControlleur.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/CommentaireControlleur.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000,http://localhost:3001",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/api/v2")
 public class CommentaireControlleur
 {
