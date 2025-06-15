c2bc06268898ac35f0dae80cbff2b7f9a270ac42
Hope Horizon <168280646+SoftwareDevelloper@users.noreply.github.com>
2025-06-15 21:35:53 +0100

    Update MailControllleur.java

--- BackendPFE/src/main/java/org/example/backendpfe/Controlleur/MailControllleur.java
+++ BackendPFE/src/main/java/org/example/backendpfe/Controlleur/MailControllleur.java
@@ -9,7 +9,7 @@ import org.springframework.mail.MailSender;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins ="*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
+@CrossOrigin(origins ="https://smartech-production-e572.up.railway.app",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/contact")
 public class MailControllleur
 {
